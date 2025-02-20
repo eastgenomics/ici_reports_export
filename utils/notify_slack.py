2aeacf428bb522bf46cafa7da53f1817fd67fc1b
RSWilson1 <robert.s.wilson1996@gmail.com>
2025-02-20 11:41:25 +0000

    added handling of request exceptions

--- utils/notify_slack.py
+++ utils/notify_slack.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
@@ -8,6 +7,17 @@ from dotenv import load_dotenv
 
 class SlackClient:
     def __init__(self):
+        required_vars = {
+         "SLACK_TOKEN": "slack_token",
+         "SLACK_LOG_CHANNEL": "slack_log_channel",
+         "SLACK_ALERT_CHANNEL": "slack_alert_channel"
+        }
+        missing_vars = [
+            env_var for env_var, attr in required_vars.items()
+            if not os.getenv(env_var)
+        ]
+        if missing_vars:
+            raise ValueError(f"Missing required environment variables: {', '.join(missing_vars)}")
         load_dotenv()  # Load environment variables
         self.webhooks = {
             "log": os.getenv("SLACK_LOG_WEBHOOK"),
@@ -41,7 +51,23 @@ class SlackClient:
             raise ValueError(f"Invalid webhook channel: {channel}")
 
         payload = {"text": message}
-        response = requests.post(webhook_url, json=payload, timeout=30)
+        try:
+            response = requests.post(webhook_url,
+                                     json=payload,
+                                     timeout=30)
+            response.raise_for_status()
+        except requests.exceptions.Timeout:
+            raise Exception("Request timed out")
+        except requests.exceptions.TooManyRedirects:
+            raise Exception("Too many redirects")
+        except requests.exceptions.ConnectionError:
+            raise Exception("Connection error occurred")
+        except requests.exceptions.HTTPError as e:
+            raise Exception(f"HTTP error occurred: {e.response.status_code} - {e.response.text}")
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Request failed: {e}")
 
         if response.status_code != 200:
             raise Exception(f"Request failed with status {response.status_code}, {response.text}")
+        else:
+            print(f"Message posted to {channel} channel")
